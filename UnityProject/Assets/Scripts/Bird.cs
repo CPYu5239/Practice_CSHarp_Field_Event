46ce998fb5d27ceed0fedc129f5ad63d7dc209c5
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-13 14:57:19 +0800

    建立小雞Script

---
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [Header("移動速度")]
+    [Range(1f,100f)]
+    public float Speed;
+
+    [Header("跳躍高度")]
+    [Range(1f, 100f)]
+    public float Jump;
+
+    [Header("對話內容")]
+    public string Talk = "咕咕咕咕~~";
+
+    [Header("是否獲得雞蛋")]
+    public bool Egg;
+
+    void Start()
+    {
+        print("遊戲開始!!!");   
+    }
+
+    
+    void Update()
+    {
+        print(Talk);
+    }
+}
